ce27b79e91df87ffe4be9e1bc2fe84e8dcd6dc56
Vladislav Antonov <vladislav.antonov@gmail.com>
2021-05-04 21:10:23 +0300

    Apply clang-tidy and cppcheck

---
+++ 3_linted/ComponentCollisionDummy.h
@@ -0,0 +1,20 @@
+#pragma once
+
+#include <cmath>
+
+#include "Component.h"
+
+class ComponentCollisionDummy : public Component {
+ public:
+  ComponentCollisionDummy(Object* parent) { m_Parent = parent; };
+  ComponentCollisionDummy(int ObjectId) { m_GameObjectId = ObjectId; }
+  ComponentCollisionDummy(){};
+  ~ComponentCollisionDummy(){};
+
+  void Update(double deltaTime = 0.013) override {
+    m_ElapsedDt += deltaTime;
+    double arbVal = cos(m_ElapsedDt) *
+                    sqrt(m_ElapsedDt * deltaTime + m_ElapsedDt * deltaTime) / 2;
+    arbVal += sin(arbVal) * cos(arbVal) * tan(arbVal);
+  }
+};
---
+++ 3_linted/README.md
@@ -0,0 +1,18 @@
+# Version 3
+
+## Changes from previous version 
+
+* Used `cmake .. -DCMAKE_EXPORT_COMPILE_COMMANDS=ON`
+* Applied `run-clang-tidy -checks='cppcoreguidelines-*,performance-*,readibility-*,modernize-*,misc-*,clang-analyzer-*' ` 
+* Applied `cppcheck  --std=c++17 *.*`
+
+## Performance 
+
+| Case | Time, ms |
+|----|---|
+| Non-cache friendly | 4036 |
+| Cache friendly | 2815 |
+
+## Executable size
+
+24352 bytes
